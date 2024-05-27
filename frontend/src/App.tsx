90b2b659a66ba2ecb3dfadf192a131cd82efd202
Max Martini <maxmmartini@gmail.com>
2024-05-27 18:54:26 -0300

    Inclusao da primeira pagina front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,23 +14,33 @@ function App() {
     // EXECUTA NO PRIMEIRO LOAD DA PAGINA
     fetch("http://localhost:3000/eventos")
     .then(res => res.json())
-    .then(data => { console.log(data); setEventos(data); })
+    .then(data => setEventos(data))
     .catch(err => console.warn(err))
 
   }, [])
 
+  function dataPtBr(a: Date){ console.log();
+    if (a) return new Date(new Date(a).toISOString().slice(0, 10)+" EDT").toLocaleString("pt-BR", { dateStyle: "short", timeStyle: undefined, timeZone: "America/Sao_Paulo" });
+  }
   return (
     <>
       <h1>Eventos</h1>
       
-      <table border="1" cellPadding="3">
-        
-        {eventos.map((e:any) => 
-          <tr key={e.id}>
-            <td>{e.nome}</td>
-            <td>{e.data}</td>
-          </tr> 
-        )}
+      <table border={1} cellPadding={10} cellSpacing={1}>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {eventos.map((e:Evento) => 
+            <tr key={e.id}>
+              <td>{e.nome}</td>
+              <td>{dataPtBr(e.data)}</td>
+            </tr> 
+          )}
+        </tbody>
         
       </table>
     </>
